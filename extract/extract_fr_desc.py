c498d0ddcb349e5e86c741c98247e671d56d18a3
sterzhang <2254854626@qq.com>
2024-04-15 19:55:28 +0800

    stage1-extract

---
+++ extract/extract_fr_desc.py
@@ -0,0 +1,156 @@
+import argparse
+import json
+import re
+import sys
+import time
+from tqdm import tqdm
+import openai
+import requests as requests
+
+"""
+input_file ("image", "description")
+output_file  
+{
+    "image": image_name,
+    "extr_obj": extr_obj,
+    "description": description
+}
+"""
+
+example1_description = """description: This image captures a cozy Christmas scene. At the center of the image is a bed adorned with a **red satin bedspread** with **gold tassels** and a **gold pillow**. The bed is nestled within **red curtains** that have **gold trim**, adding to the festive atmosphere.\n\nOn the bed, there are **four teddy bears**, two of which are brown and the other two are white. They are arranged in a pile, creating a sense of warmth and comfort. Adding to this feeling is a **white cat** that is peacefully sleeping next to the teddy bears.\n\nThe bed is further decorated with **red and gold Christmas ornaments**, enhancing the holiday spirit. To the right of the bed stands a **Christmas tree** adorned with **gold ornaments**, while on the left side of the bed, there are **two red candlesticks**.\n\nOverall, this image exudes a warm and festive atmosphere, perfect for the holiday season."""
+example1_response = "response: red satin bedspread. gold tassels. gold pillow. red curtains. gold trim. four teddy bears. white cat. red and gold Christmas ornaments. Christmas tree. gold ornaments. two red candlesticks."
+
+example2_description = """description: The image depicts a lively scene in an ornately decorated room. At the center of the image stands a man with a long white beard, wearing a tall, cylindrical black hat with a flat top. He is dressed in a white shirt paired with a green tie and carries a black backpack. 
+The room is bustling with people, some of whom are wearing green shirts, adding to the vibrant atmosphere. The room itself is richly adorned, featuring a gold ceiling and red columns that exude an air of grandeur. 
+The man, despite being in the midst of the crowd, stands out due to his unique attire and commanding presence. The people are visible on all sides of him, suggesting that he is at the heart of this gathering."""
+example2_response = "response: man with a long white beard. tall cylindrical black hat. white shirt. green tie. black backpack. people wearing green shirts. gold ceiling. red columns."
+
+example3_description = """description: This image captures a charming scene in a bedroom. The main subject is a **black cat** standing on its hind legs on a **gray nightstand**. The cat, full of curiosity, is reaching its paw into a **white cup** adorned with blue and green designs.\n\nThe nightstand hosts a few other items: a **beige lamp**, a **white tissue box** with blue flowers, and a **pink jar of Vaseline**. Each item is neatly arranged on the nightstand, creating a harmonious tableau.\n\nIn the background, you can see a bed covered with a **white comforter** and adorned with a **yellow pillow**, adding a pop of color to the scene. The precise location of these objects and their relative positions contribute to the overall composition of the image, creating a snapshot of everyday life with a hint of feline mischief."""
+example3_response = "response: black cat. gray nightstand. white cup. beige lamp. white tissue box. blue flowers. pink jar of Vaseline. white comforter. yellow pillow."
+
+
+def load_api_keys_from_txt(key_path):
+    key_list = []
+    with open(key_path, 'r') as file:
+        for line in file:
+            matches = re.findall(r'sk-\w+', line)
+            key_list += matches
+    return key_list
+
+def query_ChatGPT(input_data, count, code_api_list):
+    response = None
+    received = False
+    while not received:
+        try:
+            openai.api_key = code_api_list[count % len(code_api_list)]
+
+            response = openai.ChatCompletion.create(
+                model="gpt-3.5-turbo-0613",
+                messages=input_data,
+                temperature=1.0,
+            )
+            received = True
+        except openai.error.InvalidRequestError as e:
+            print(f"InvalidRequestError\nPrompt passed in:\n\n{input_data}\n\n")
+            raise e
+        except openai.error.APIError as e:
+            print("API error:", e)
+            print(code_api_list[count % len(code_api_list)])
+            count += 1
+            # time.sleep(2)
+    return response, count
+
+def query_ChatGPT_free(question):
+    api_key = "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"
+    headers = {
+        "Authorization": 'Bearer ' + api_key,
+    }
+    params = {
+        "messages": [
+            {
+                "role": 'user',
+                "content": question
+            }
+        ],
+        "model": 'gpt-3.5-turbo'
+    }
+    response = requests.post(
+        "https://aigptx.top/v1/chat/completions",
+        headers=headers,
+        json=params,
+        stream=False
+    )
+    res = response.json()
+    res_content = res['choices'][0]['message']['content']
+    return res
+
+
+def main(args):
+    total_lines = sum(1 for _ in open(args.input_file_path))
+
+    with open(args.api_key_path, 'r') as key_file:
+        code_api_list = load_api_keys_from_txt(args.api_key_path)
+
+    with open(args.input_file_path, 'r') as input_file:
+        with open(args.output_file_path, 'a') as output_file:
+            for i, line in enumerate(tqdm(input_file, total=total_lines, desc="Stage1.2 - extract objects from the description")):
+                if i < args.start_line:
+                    continue  # Skip lines until the start_line is reached
+                json_obj = json.loads(line)
+                image_name = json_obj.get("image")
+                description = json_obj.get("description")
+
+                extr_prompt = f"""\
+                Your are a helpful entities extractor! Please help me to extract common entities in the given description of an image. Remember, you should only help me extract common entities that truly correspond to the objects in the image. Avoid extracting abstract or non-specific entities (such as "cozy atmosphere", "excitement", "sky view")!!! Your response should strictly follow below format:
+
+                obj1. obj2. obj3 ….
+
+                Here are some examples for your reference.
+            
+                example1:
+                {example1_description}
+                {example1_response}
+                ---
+
+                example2:
+                {example2_description}
+                {example2_response}
+                ---
+
+                example3:
+                {example3_description}
+                {example3_response}
+                ---
+
+                description: {description}
+                response:"""
+
+                response = query_ChatGPT_free(extr_prompt)
+                time.sleep(0.5)
+
+                extr_obj = response.split(". ")
+
+                output_data = {
+                    "image": image_name,
+                    "extr_obj": extr_obj,
+                    "description": description
+                }
+                output_file.write(json.dumps(output_data) + '\n')
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Extract objects from image descriptions using OpenAI ChatGPT.")
+    parser.add_argument("--input_file_path", type=str, default="./annotation/tmp.jsonl", help="Path to input file containing image descriptions.")
+    parser.add_argument("--output_file_path", type=str, default="./annotation/stage1/obj_extr_from_desc.jsonl", help="Path to output file to store extracted objects.")
+    parser.add_argument("--api_key_path", type=str, default="api_key.txt", help="Path to file containing OpenAI API keys.")
+    parser.add_argument("--start_line", type=int, default=0, help="Start reading the input file from this line number (if fail in line x, then set this value as x to continue generate).")
+
+    args = parser.parse_args()
+    main(args)
+
+
+"""
+python stage1/obj_extract_from_desc.py --input_file_path /home/zhangjianshu/Mercury/llava/llava_stage0_1.jsonl \
+    --output_file_path /home/zhangjianshu/Mercury/llava/llava_stage1_desc.jsonl \
+    --api_key_path api_key.txt \
+    --start_line 0
+"""
---
+++ extract/extract_fr_img.py
@@ -0,0 +1,180 @@
+import argparse
+import multiprocessing as mp
+import os
+import time
+import cv2
+import tqdm
+import sys
+import json
+
+from detectron2.config import get_cfg
+from detectron2.data.detection_utils import read_image
+from detectron2.utils.logger import setup_logger
+
+sys.path.insert(0, 'extract/third_party/CenterNet2/projects/CenterNet2/')
+from centernet.config import add_centernet_config
+from grit.config import add_grit_config
+
+from grit.predictor import VisualizationDemo
+import warnings
+
+warnings.filterwarnings("ignore") 
+WINDOW_NAME = "Extract objects from image"
+
+"""
+input_file ("image")
+output_file ("image", "objects", "bounding_boxes")
+gpu 12000M
+"""
+
+"""
+python extract/extract_fr_img.py \
+    --test_task DenseCap \
+    --config_file ./stage1/configs/GRiT_B_DenseCap_ObjectDet.yaml \
+    --confidence_threshold 0.5 \
+    --image_folder  \
+    --input_file  \
+    --output_file  \
+    --opts MODEL.WEIGHTS ./ckpt/grit_b_densecap_objectdet.pth
+"""
+
+
+
+def setup_cfg(args):
+    cfg = get_cfg()
+    if args.cpu:
+        cfg.MODEL.DEVICE="cpu"
+    add_centernet_config(cfg)
+    add_grit_config(cfg)
+    cfg.merge_from_file(args.config_file)
+    cfg.merge_from_list(args.opts)
+    # Set score_threshold for builtin models
+    cfg.MODEL.ROI_HEADS.SCORE_THRESH_TEST = args.confidence_threshold
+    cfg.MODEL.PANOPTIC_FPN.COMBINE.INSTANCES_CONFIDENCE_THRESH = args.confidence_threshold
+    if args.test_task:
+        cfg.MODEL.TEST_TASK = args.test_task
+    cfg.MODEL.BEAM_SIZE = 1
+    cfg.MODEL.ROI_HEADS.SOFT_NMS_ENABLED = False
+    cfg.USE_ACT_CHECKPOINT = False
+    cfg.freeze()
+    return cfg
+
+def get_parser():
+    parser = argparse.ArgumentParser(description="Detectron2 demo for builtin configs")
+    parser.add_argument(
+        "--config_file",
+        default="./extract/configs/GRiT_B_DenseCap_ObjectDet.yaml",
+        metavar="FILE",
+        help="path to config file",
+    )
+    parser.add_argument("--cpu", action='store_true', help="Use CPU only.")
+    parser.add_argument(
+        "--image_folder",
+        type=str,
+        help="A list of space separated input images; "
+        "or a single glob pattern such as 'directory/*.jpg'",
+    )
+    parser.add_argument(
+        "--visualize_output",
+        type=str,
+        default="false",
+        help="A file or directory to save output visualizations. "
+        "If not given, will show output in an OpenCV window.",
+    )
+    parser.add_argument(
+        "--confidence_threshold",
+        type=float,
+        default=0.5,
+        help="Minimum score for instance predictions to be shown",
+    )
+    parser.add_argument(
+        "--test_task",
+        type=str,
+        default='DenseCap',
+        help="Choose a task to have GRiT perform",
+    )
+    parser.add_argument(
+        "--opts",
+        help="Modify config options using the command-line 'KEY VALUE' pairs",
+        default=[],
+        nargs=argparse.REMAINDER,
+    )
+    parser.add_argument(
+        "--input_file",
+        type=str,
+    )
+    parser.add_argument(
+        "--output_file",
+        type=str,
+    )
+    parser.add_argument(
+        "--start_line",
+        type=int,
+    )
+    parser.add_argument(
+        "--end_line",
+        type=int,
+    )
+    return parser
+
+original_stdout = sys.stdout
+sys.stdout = open(os.devnull, 'w')
+
+if __name__ == "__main__":
+    mp.set_start_method("spawn", force=True)
+    args = get_parser().parse_args()
+    setup_logger(name="fvcore")
+    logger = setup_logger()
+    logger.info("Arguments: " + str(args))
+
+    cfg = setup_cfg(args)
+
+    demo = VisualizationDemo(cfg)
+    sys.stdout = original_stdout
+    input_file = args.input_file
+    output_file = args.output_file
+    
+    output_folder = os.path.dirname(output_file)
+    if not os.path.exists(output_folder):
+        os.makedirs(output_folder)
+
+    total_lines = args.end_line - args.start_line
+    if args.image_folder:
+        with open(output_file, 'a') as f:
+            with open(input_file, 'r') as json_file:
+                for i, line in enumerate(tqdm.tqdm(json_file, total=total_lines, desc="Extract objects from the image")):
+                    if i < args.start_line:
+                        continue
+                    if i >= args.end_line:
+                        break
+                    data = json.loads(line)
+                    image_path = data['image']
+                    img = read_image(os.path.join(args.image_folder, image_path), format="BGR")
+                    predictions, visualized_output = demo.run_on_image(img)
+
+                    # bounding box in image -> details in text format
+                    boxes = predictions["instances"].pred_boxes if predictions["instances"].has("pred_boxes") else None
+                    object_description = predictions["instances"].pred_object_descriptions.data
+                    # create lists to store object descriptions and bounding boxes
+                    object_list = []
+                    bounding_box_list = []
+
+                    for i in range(len(object_description)):
+                        object_list.append(object_description[i])
+                        box = [int(a) for a in boxes[i].tensor.cpu().detach().numpy()[0]]
+                        bounding_box_list.append(box)
+
+                    # construct data dictionary
+                    data = {'image': image_path, 'objects': object_list, 'bounding_boxes': bounding_box_list}
+                    f.write(json.dumps(data) + '\n')
+
+                    if args.visualize_output:
+                        if args.visualize_output.lower() == 'false':
+                            continue  # Skip saving visualizations if output is set to false
+
+                        if not os.path.exists(args.visualize_output):
+                            os.mkdir(args.visualize_output)
+                        if os.path.isdir(args.visualize_output):
+                            assert os.path.isdir(args.visualize_output), args.visualize_output
+                            out_filename = os.path.join(args.visualize_output, image_path)
+                        visualized_output.save(out_filename)
